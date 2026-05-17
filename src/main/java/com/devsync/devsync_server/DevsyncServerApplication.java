d479fd23c4f0170b02bc3153808a655e5eba8265
MGhiremath0281 <muttuh028@gmail.com>
2026-05-17 23:46:12 +0530

    Initial commit

---
+++ src/main/java/com/devsync/devsync_server/DevsyncServerApplication.java
@@ -0,0 +1,13 @@
+package com.devsync.devsync_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsyncServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsyncServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsync/devsync_server/DevsyncServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsync.devsync_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsyncServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
