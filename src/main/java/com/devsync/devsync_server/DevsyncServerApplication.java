81a7c4af626fc7e8531d8177d0c7845ccd6a81f3
MGhiremath0281 <muttuh028@gmail.com>
2026-05-18 20:03:56 +0530

    chore: minor updates in main application class

--- src/main/java/com/devsync/devsync_server/DevsyncServerApplication.java
+++ src/main/java/com/devsync/devsync_server/DevsyncServerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevsyncServerApplication {
+public class  DevsyncServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevsyncServerApplication.class, args);
