54ddbc8f954c5bd5168dcf3c34086aa6b002d124
MGhiremath0281 <muttuh028@gmail.com>
2026-05-26 13:31:22 +0530

    feat: add meeting persistence repositories

--- src/main/java/com/devsync/devsync_server/meeting/repository/MeetingParticipantRepository.java
+++ src/main/java/com/devsync/devsync_server/meeting/repository/MeetingParticipantRepository.java
@@ -1,4 +1,16 @@
 package com.devsync.devsync_server.meeting.repository;
 
-public class MeetingParticipantRepository {
+import com.devsync.devsync_server.meeting.model.MeetingParticipant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MeetingParticipantRepository extends JpaRepository<MeetingParticipant, Long> {
+
+    List<MeetingParticipant> findByMeetingId(Long meetingId);
+
+    Optional<MeetingParticipant> findByMeetingIdAndUserId(Long meetingId, Long userId);
+
+    long countByMeetingId(Long meetingId);
 }
