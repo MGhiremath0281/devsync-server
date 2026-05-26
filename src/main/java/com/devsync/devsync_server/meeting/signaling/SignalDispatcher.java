e66c1f4469922a9879eb671f1eed4aed32eabeb1
MGhiremath0281 <muttuh028@gmail.com>
2026-05-26 13:32:49 +0530

    feat: implement webrtc signaling dispatcher

--- src/main/java/com/devsync/devsync_server/meeting/signaling/SignalDispatcher.java
+++ src/main/java/com/devsync/devsync_server/meeting/signaling/SignalDispatcher.java
@@ -1,4 +1,32 @@
 package com.devsync.devsync_server.meeting.signaling;
 
+import com.devsync.devsync_server.meeting.dto.MeetingSignalDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class SignalDispatcher {
+
+    private final WebRTCSignalingService signalingService;
+
+    public void dispatch(MeetingSignalDto signal) {
+
+        switch (signal.getSignalType()) {
+
+            case "OFFER" ->
+                    signalingService.handleOffer(signal);
+
+            case "ANSWER" ->
+                    signalingService.handleAnswer(signal);
+
+            case "ICE_CANDIDATE" ->
+                    signalingService.handleIceCandidate(signal);
+
+            default ->
+                    throw new IllegalArgumentException(
+                            "Unsupported signal type"
+                    );
+        }
+    }
 }
--- src/main/java/com/devsync/devsync_server/meeting/signaling/WebRTCSignalingService.java
+++ src/main/java/com/devsync/devsync_server/meeting/signaling/WebRTCSignalingService.java
@@ -1,4 +1,57 @@
 package com.devsync.devsync_server.meeting.signaling;
 
+import com.devsync.devsync_server.meeting.dto.MeetingSignalDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class WebRTCSignalingService {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public void handleOffer(MeetingSignalDto signal) {
+
+        log.info(
+                "WebRTC OFFER from user {} for meeting {}",
+                signal.getSenderId(),
+                signal.getMeetingId()
+        );
+
+        messagingTemplate.convertAndSend(
+                "/topic/meeting/" + signal.getMeetingId() + "/signals",
+                signal
+        );
+    }
+
+    public void handleAnswer(MeetingSignalDto signal) {
+
+        log.info(
+                "WebRTC ANSWER from user {} for meeting {}",
+                signal.getSenderId(),
+                signal.getMeetingId()
+        );
+
+        messagingTemplate.convertAndSend(
+                "/topic/meeting/" + signal.getMeetingId() + "/signals",
+                signal
+        );
+    }
+
+    public void handleIceCandidate(MeetingSignalDto signal) {
+
+        log.info(
+                "ICE Candidate from user {} for meeting {}",
+                signal.getSenderId(),
+                signal.getMeetingId()
+        );
+
+        messagingTemplate.convertAndSend(
+                "/topic/meeting/" + signal.getMeetingId() + "/signals",
+                signal
+        );
+    }
 }
