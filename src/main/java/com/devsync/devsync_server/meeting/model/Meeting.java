9f02b1c651b9d1854deadc17a23f57d90002395b
MGhiremath0281 <muttuh028@gmail.com>
2026-05-26 11:46:40 +0530

    feat: add meeting domain models and repositories

---
+++ src/main/java/com/devsync/devsync_server/meeting/model/Meeting.java
@@ -0,0 +1,4 @@
+package com.devsync.devsync_server.meeting.model;
+
+public class Meeting {
+}
---
+++ src/main/java/com/devsync/devsync_server/meeting/repository/MeetingRepository.java
@@ -0,0 +1,4 @@
+package com.devsync.devsync_server.meeting.repository;
+
+public class MeetingRepository {
+}
