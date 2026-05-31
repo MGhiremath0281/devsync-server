fd0d5f6fe84d70d12b7422d60ab3de0a433cb28c
MGhiremath0281 <muttuh028@gmail.com>
2026-05-31 19:06:04 +0530

    feat: add task comment persistence

--- src/main/java/com/devsync/devsync_server/task/repository/TaskCommentRepository.java
+++ src/main/java/com/devsync/devsync_server/task/repository/TaskCommentRepository.java
@@ -1,4 +1,10 @@
 package com.devsync.devsync_server.task.repository;
 
-public class TaskCommentRepository {
+import com.devsync.devsync_server.task.model.TaskComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskCommentRepository extends JpaRepository<TaskComment, Long> {
+    List<TaskComment> findByTaskIdOrderByCreatedAtAsc(Long taskId);
 }
