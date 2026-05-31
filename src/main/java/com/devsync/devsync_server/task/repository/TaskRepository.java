9dc40addf3eaa0c85b1136d7633e9e0eb77128b1
MGhiremath0281 <muttuh028@gmail.com>
2026-05-31 19:04:41 +0530

    feat: create task repository layer

--- src/main/java/com/devsync/devsync_server/task/repository/TaskRepository.java
+++ src/main/java/com/devsync/devsync_server/task/repository/TaskRepository.java
@@ -1,4 +1,14 @@
 package com.devsync.devsync_server.task.repository;
 
-public class TaskRepository {
+import com.devsync.devsync_server.task.model.Task;
+import com.devsync.devsync_server.task.model.TaskStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByTeamId(Long teamId);
+    List<Task> findByAssigneeId(Long assigneeId);
+    List<Task> findByTeamIdAndStatus(Long teamId, TaskStatus status);
+    List<Task> findByReporterId(Long reporterId);
 }
