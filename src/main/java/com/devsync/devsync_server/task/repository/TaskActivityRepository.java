ab2c6c22ef237ba21ec49de72d84a009ae245e5e
MGhiremath0281 <muttuh028@gmail.com>
2026-05-31 19:07:03 +0530

    feat: add task activity tracking

--- src/main/java/com/devsync/devsync_server/task/repository/TaskActivityRepository.java
+++ src/main/java/com/devsync/devsync_server/task/repository/TaskActivityRepository.java
@@ -1,4 +1,10 @@
 package com.devsync.devsync_server.task.repository;
 
-public class TaskActivityRepository {
+import com.devsync.devsync_server.task.model.TaskActivity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskActivityRepository extends JpaRepository<TaskActivity, Long> {
+    List<TaskActivity> findByTaskIdOrderByCreatedAtDesc(Long taskId);
 }
