86104e1e357a65565c01be52e82dd4633abb5cf7
MGhiremath0281 <muttuh028@gmail.com>
2026-05-31 19:06:13 +0530

    feat(comment): implement comment DTOs

--- src/main/java/com/devsync/devsync_server/task/dto/request/CreateCommentRequest.java
+++ src/main/java/com/devsync/devsync_server/task/dto/request/CreateCommentRequest.java
@@ -1,4 +1,15 @@
 package com.devsync.devsync_server.task.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class CreateCommentRequest {
+
+    @NotNull(message = "Author ID is required")
+    private Long authorId;
+
+    @NotBlank(message = "Content is required")
+    private String content;
 }
