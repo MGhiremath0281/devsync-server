e2489970d2a3308b54d20b23c484cb16011f127a
MGhiremath0281 <muttuh028@gmail.com>
2026-05-31 19:05:23 +0530

    feat: define task service contracts

--- src/main/java/com/devsync/devsync_server/task/service/TaskService.java
+++ src/main/java/com/devsync/devsync_server/task/service/TaskService.java
@@ -1,4 +1,35 @@
 package com.devsync.devsync_server.task.service;
 
-public class TaskService {
+import com.devsync.devsync_server.task.dto.request.AssignTaskRequest;
+import com.devsync.devsync_server.task.dto.request.CreateTaskRequest;
+import com.devsync.devsync_server.task.dto.request.UpdateStatusRequest;
+import com.devsync.devsync_server.task.dto.request.UpdateTaskRequest;
+import com.devsync.devsync_server.task.dto.response.TaskActivityResponse;
+import com.devsync.devsync_server.task.dto.response.TaskResponse;
+import com.devsync.devsync_server.task.dto.response.TaskSummaryResponse;
+import com.devsync.devsync_server.task.model.TaskStatus;
+
+import java.util.List;
+
+public interface TaskService {
+
+    TaskResponse createTask(CreateTaskRequest request);
+
+    TaskResponse getTaskById(Long taskId);
+
+    List<TaskSummaryResponse> getTasksByTeam(Long teamId);
+
+    List<TaskSummaryResponse> getTasksByAssignee(Long assigneeId);
+
+    List<TaskSummaryResponse> getTasksByTeamAndStatus(Long teamId, TaskStatus status);
+
+    TaskResponse updateTask(Long taskId, UpdateTaskRequest request);
+
+    TaskResponse assignTask(Long taskId, AssignTaskRequest request);
+
+    TaskResponse updateStatus(Long taskId, UpdateStatusRequest request);
+
+    void deleteTask(Long taskId);
+
+    List<TaskActivityResponse> getTaskActivities(Long taskId);
 }
