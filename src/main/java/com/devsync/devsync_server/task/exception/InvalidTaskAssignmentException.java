c0f597aaa4c0d644b72b2e392624655f7a740b5f
MGhiremath0281 <muttuh028@gmail.com>
2026-05-31 19:07:13 +0530

    feat: add custom task exceptions

--- src/main/java/com/devsync/devsync_server/task/exception/InvalidTaskAssignmentException.java
+++ src/main/java/com/devsync/devsync_server/task/exception/InvalidTaskAssignmentException.java
@@ -1,4 +1,7 @@
 package com.devsync.devsync_server.task.exception;
 
-public class InvalidTaskAssignmentException {
+public class InvalidTaskAssignmentException extends RuntimeException {
+    public InvalidTaskAssignmentException(String message) {
+        super(message);
+    }
 }
