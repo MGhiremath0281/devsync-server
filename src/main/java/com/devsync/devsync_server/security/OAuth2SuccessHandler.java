120bc6e32db5ff3ffd6dbdd5fa596daca1645abb
MGhiremath0281 <muttuh028@gmail.com>
2026-06-02 11:04:55 +0530

    feat: implement OAuth2 login processing and JWT issuance

--- src/main/java/com/devsync/devsync_server/security/OAuth2SuccessHandler.java
+++ src/main/java/com/devsync/devsync_server/security/OAuth2SuccessHandler.java
@@ -1,4 +1,46 @@
 package com.devsync.devsync_server.security;
 
-public class OAuth2SuccessHandler {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JwtService jwtService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void onAuthenticationSuccess(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication
+    ) throws IOException {
+
+        OAuth2UserPrincipal principal = (OAuth2UserPrincipal) authentication.getPrincipal();
+
+        String jwt = jwtService.generateToken(principal.getUser());
+
+        Map<String, Object> responseData = new HashMap<>();
+        responseData.put("message", "Login Successful!");
+        responseData.put("token", jwt);
+        responseData.put("email", principal.getUser().getEmail());
+        responseData.put("username", principal.getUser().getUsername());
+        responseData.put("role", principal.getUser().getRole().name());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        response.getWriter().write(objectMapper.writeValueAsString(responseData));
+        response.getWriter().flush();
+    }
 }
