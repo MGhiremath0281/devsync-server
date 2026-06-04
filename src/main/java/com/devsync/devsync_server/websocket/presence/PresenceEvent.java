a47ffe7673e2320599610ae22c66501cb02772ff
MGhiremath0281 <muttuh028@gmail.com>
2026-06-05 00:49:40 +0530

    feat: introduce presence event payload model

--- src/main/java/com/devsync/devsync_server/websocket/presence/PresenceEvent.java
+++ src/main/java/com/devsync/devsync_server/websocket/presence/PresenceEvent.java
@@ -1,4 +1,19 @@
-package com.devsync.devsync_server.workspace.dto;
+package com.devsync.devsync_server.websocket.presence;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PresenceEvent {
+
+    private Long userId;
+
+    private Long teamId;
+
+    private boolean online;
 }
