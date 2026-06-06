ba8501e3a81f019dfc699232d3136c229fe9f73c
MGhiremath0281 <muttuh028@gmail.com>
2026-06-06 11:41:18 +0530

    feat: configure OpenTelemetry SDK and exporters

--- src/main/java/com/devsync/devsync_server/config/TelemetryConfig.java
+++ src/main/java/com/devsync/devsync_server/config/TelemetryConfig.java
@@ -1,4 +1,21 @@
 package com.devsync.devsync_server.config;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ServerHttpObservationFilter;
+
+@Configuration
 public class TelemetryConfig {
+
+    @Bean
+    public ServerHttpObservationFilter httpObservationFilter(ObservationRegistry registry) {
+        return new ServerHttpObservationFilter(registry);
+    }
+
+    @Bean
+    public ObservedAspect observedAspect(ObservationRegistry registry) {
+        return new ObservedAspect(registry);
+    }
 }
